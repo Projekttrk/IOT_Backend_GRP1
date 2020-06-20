2126ea13af1362b622258a4d61bea89ba0f0dbf2
first <jo.bro.abbo@gmail.com>
2020-06-20 15:04:00 +0200

    Excel fertig

--- processing.py
+++ processing.py
@@ -1,16 +1,20 @@
 from datetime import datetime
 import pandas as pd
 
-def process_request(json_request):
-    error_code = json_request['error'].split('https://iib.solutions/')[1]
-    plant = json_request['plant']
-    version = str(json_request['version'])
-    logtime = create_timestamp()
-    log_request('test.txt', logtime, plant, version, error_code, '\n')
 
-    export = {'Error_code': [error_code],'Plant': [Plant], 'Version': [version], 'Timestamp': [logtime]}
-    df = pd.DataFrame(export, columns=['Error_code', 'Plant','Version','Timestamp'])
-    df.to_excel(r"C:\Users\Jovan\Desktop\export_dataframe.xlsx", index=False, header=True)
+def process_request(json_request):
+    codenummer = int(json_request['error'].split(' ')[1])
+    codefarbe = json_request['error'].split(' ')[3]
+    logtime = create_timestamp()
+    log_request('test.txt', logtime, codenummer, codefarbe, '\n')
+
+    export = {'Error_code': [codenummer],'Codefarbe': [codefarbe], 'Timestamp': [logtime]}
+    df = pd.read_excel("export_dataframe.xlsx", encoding= "CP1252")
+    df2 = pd.DataFrame.from_dict(export)
+    df = df.append(df2)
+    print(df)
+
+    df.to_excel(r"export_dataframe.xlsx", index=False, header=True)
 
 
 def log_request(path, *args):
