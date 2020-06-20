c51d8dfd47123d935cf916a25b0bc4c6358745d3
first <jo.bro.abbo@gmail.com>
2020-06-20 15:45:20 +0200

    Excel fertig

--- processing.py
+++ processing.py
@@ -1,14 +1,24 @@
 from datetime import datetime
 import pandas as pd
+import configparser
 
 
 def process_request(json_request):
-    codenummer = int(json_request['error'].split(' ')[1])
+    config = configparser.ConfigParser()
+    config.read('application/error_conf.ini')
+    codenummer = json_request['error'].split(' ')[1]
     codefarbe = json_request['error'].split(' ')[3]
     logtime = create_timestamp()
+    date = logtime.split(' ')[0]
+    time = logtime.split(' ')[1]
     log_request('test.txt', logtime, codenummer, codefarbe, '\n')
 
-    export = {'Error_code': [codenummer],'Codefarbe': [codefarbe], 'Timestamp': [logtime]}
+    error_code = config['errorcodes'][codenummer]
+    prefix = config['Prefix'][codenummer]
+    codetype = config['codetype'][codenummer]
+    location = config['location'][codenummer]
+    faultcode = config['faultcode'][codenummer]
+    export = [[prefix],[codetype],[location],[faultcode],["Error"],[error_code],[date],[time],['new']]
     df = pd.read_excel("export_dataframe.xlsx", encoding= "CP1252")
     df2 = pd.DataFrame.from_dict(export)
     df = df.append(df2)
@@ -20,11 +30,11 @@ def process_request(json_request):
 def log_request(path, *args):
     with open(path, 'a') as f:
         for arg in args:
-            f.write(arg)
+            f.write(str(arg))
 
 
 def create_timestamp():
     dateTimeObj = datetime.now()
-    return dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+    return dateTimeObj.strftime("%d.%b.%Y (%H:%M:%S)")
 
 
